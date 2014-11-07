83f1830a590cda36a8a7b29f04c04fdfdeb28825
Eduardo del Nogal <edunogalj@gmail.com>
2014-11-07 12:20:09 +0100

    Add licences

--- DriverTIVA/Configuration.h
+++ DriverTIVA/Configuration.h
@@ -1,12 +1,21 @@
-/*
- * Configuration.h
- *
- *  Created on: 29/03/2014
- *      Author: Admin
- *      Estructuras de administraccion de los perifericos
- *      Falta la compatibilidad entre multiplexacion de perifericos en pines
- *
- */
+/*  Configuration.h
+    Copyright (C) 2012-2013 Eduardo del Nogal Jiménez
+
+    This is part of DriverTiva.
+
+    DriverTiva is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DriverTiva is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DriverTiva.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef __CONFIGURATION_H__
 #define __CONFIGURATION_H__
--- DriverTIVA/Funciones.h
+++ DriverTIVA/Funciones.h
@@ -1,9 +1,21 @@
-/*
- * Funciones.h
- *
- *  Created on: 01/10/2014
- *      Author: User
- */
+/*  Funciones.h
+    Copyright (C) 2012-2013 Eduardo del Nogal Jiménez
+
+    This is part of DriverTiva.
+
+    DriverTiva is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DriverTiva is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DriverTiva.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef  __DRIVERTIVA_FUNCIONES_H_
 #define  __DRIVERTIVA_FUNCIONES_H_
